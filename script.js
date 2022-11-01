fac971f09e9c223416ac33990eedeaba6224e1a1
joshubelz25 <joshubelz25@g.crookcountyschools.org>
2022-11-01 13:43:04 -0700

    Loops

---
+++ script.js
@@ -0,0 +1,28 @@
+ console.log("HELLLO JR")
+
+const people = ("Deez ", "Nuts ", "Deez Nuts ");
+
+let i = 0;
+while(people[i]){
+console.log(people[i]);
+i++;
+}
+
+for(i=i; i<=21; i=i+21){
+console.log(i*21);
+i++;
+}
+
+const number = ("210 ", "250 ", "2300 ");
+
+document.write("<h1> Numbers </h2>")
+
+
+for(i=0; i<number.length; i++){
+document.write(number[i])
+}
+
+while(i<=10){
+console.log("its equal to 10")
+i++;
+}
